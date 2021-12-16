810cc8489071d29464123a3dd64132164deea80f
FlorianMgs <madgic.shrooms@gmail.com>
2021-12-16 11:17:58 +0100

    Permissions almost ok

--- softdesk/api/views.py
+++ softdesk/api/views.py
@@ -1,7 +1,6 @@
 from rest_framework.views import APIView
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.response import Response
-from rest_framework.permissions import IsAuthenticated
 from rest_framework import status
 from django.db import transaction, IntegrityError
 from django.contrib.auth import get_user_model
